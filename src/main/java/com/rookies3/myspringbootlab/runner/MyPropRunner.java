1f517efc6d2de890a990b8bca196ce556a3ace1c
lemongza <92498054+lemongza@users.noreply.github.com>
2025-05-15 20:10:49 +0900

    [실습2-1] MyPropProperties 클래스를 작성하기

--- src/main/java/com/rookies3/myspringbootlab/runner/MyPropRunner.java
+++ src/main/java/com/rookies3/myspringbootlab/runner/MyPropRunner.java
@@ -1,5 +1,7 @@
 package com.rookies3.myspringbootlab.runner;
 
+import com.rookies3.myspringbootlab.property.MyPropProperties;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -14,9 +16,16 @@ public class MyPropRunner implements ApplicationRunner {
     @Value("${myprop.port}")
     private int port;
 
+    @Autowired
+    private MyPropProperties properties;
+
     @Override
     public void run(ApplicationArguments args) throws Exception {
         System.out.println("${myprop.username}: " + username);
         System.out.println("${myprop.port}: " + port);
+
+        // MyPropProperties 객체를 주입(Injection) 받아서 getter 메서드를 출력하기
+        System.out.println("MyPropProperties.getUsername: " + properties.getUsername());
+        System.out.println("MyPropProperties.getPort: " + properties.getPort());
     }
 }
